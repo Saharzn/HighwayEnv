9101b13ef30a946b48bc1b80405ee11397327fb1
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-05 09:55:48 +0200

    Update abstract.py

--- highway_env/envs/common/abstract.py
+++ highway_env/envs/common/abstract.py
@@ -174,7 +174,7 @@ class AbstractEnv(gym.Env):
         """
         info = [self.vehicle.speed, self.vehicle.crashed, action, self.time]
         try:
-            info.append = self._rewards(action)
+            info.append(self._rewards(action))
         except NotImplementedError:
             pass
         return info
