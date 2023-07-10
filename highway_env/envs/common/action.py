5f4a371605a90649ed83290357dda1b3b07412d8
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-10 19:40:36 +0200

    Update action.py

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -135,25 +135,11 @@ class ContinuousAction(ActionType):
             action[0] = np.clip(action[0], -1, 1)
         if self.speed_range:
             self.controlled_vehicle.MIN_SPEED, self.controlled_vehicle.MAX_SPEED = self.speed_range
-        
-        
-        if self.action_lat == "LANE_RIGHT":
-                    _from, _to, _id = self.target_lane_index
-                    target_lane_index = _from, _to, np.clip(_id + 1, 0, len(self.road.network.graph[_from][_to]) - 1)
-                    if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
-                          target_lane_index_f = target_lane_index
-        if self.action_lat == "LANE_LEFT":
-                    _from, _to, _id = self.target_lane_index
-                    target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
-                    if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
-                          target_lane_index_f = target_lane_index
-        
-        
-        
+           
         if self.longitudinal and self.lateral:
             self.controlled_vehicle.act({
                 "acceleration_index": utils.lmap(action[0], [-1, 1], self.acceleration_range),
-                "steering": CV.steering_control(0),
+                "steering": CV.steering_control(CV.target_lane_index),
             })
         self.last_action = action
     
