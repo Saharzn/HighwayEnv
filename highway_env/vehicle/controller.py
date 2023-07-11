21f859a5e3b23cc755173d23b6dd054fa7209b3d
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-11 23:31:47 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -100,7 +100,7 @@ class ControlledVehicle(Vehicle):
             if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
                 self.target_lane_index = target_lane_index
 
-        action = {"steering": self.steering_control(self.target_lane_index),
+        action = {"steering": self.steering_control("LANE_LEFT"),
                   "acceleration": self.speed_control(self.target_speed)}
         action['steering'] = np.clip(action['steering'], -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE)
         super().act(action)
