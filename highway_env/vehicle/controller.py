a27a778052ee0804b2c90014413268e8ef21e8d0
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-11 09:49:30 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -92,7 +92,7 @@ class ControlledVehicle(Vehicle):
             target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
             if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
                 self.target_lane_index = target_lane_index
-        return(self.target_lane_index)
+        return(self.self.lane_index)
     
     def act(self, action: Union[dict, str] = None) -> None:
         """
