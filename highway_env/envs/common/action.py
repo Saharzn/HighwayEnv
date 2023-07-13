137627ce0fb9e9a30cbe03c78427d78856cb5bf0
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-14 00:11:16 +0200

    Update action.py

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -155,9 +155,8 @@ class ContinuousAction_s(ActionType):
                     and self.lateral:
                   actions[1] = 'LANE_RIGHT'
             
-            self.controlled_vehicle.act(actions)
-        action = {"steering": 0.1,
-                  "acceleration": utils.lmap(actions[0], [-1, 1], self.acceleration_range)}     
+            self.controlled_vehicle.act({"steering": 0.1,
+                  "acceleration": utils.lmap(actions[0], [-1, 1], self.acceleration_range)})     
         self.last_actions = actions
     
 class DiscreteMetaAction(ActionType):
