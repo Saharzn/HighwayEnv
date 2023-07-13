a7a3f3f0adfe7b8270950914de520e0d39a6e8e5
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-13 14:01:17 +0200

    Update action.py

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -154,8 +154,8 @@ class ContinuousAction_s(ActionType):
             target_index = CV.index_s(self,action)
             self.controlled_vehicle.act({
                 "acceleration": utils.lmap(action[0], [-1, 1], self.acceleration_range),
-                "steering": CV.self.steering_control(target_index)
-                #"steering":0.1,
+                #"steering": CV.self.steering_control(target_index)
+                "steering":0.1,
             })
         self.last_action = action
     
