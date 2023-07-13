038bf887f46088f190a43edec62f44cccf35dee6
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-13 23:25:04 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -86,7 +86,6 @@ class ControlledVehicle(Vehicle):
         """
         ACCELERATION_RANGE = (-5, 5.0)
         acceleration_range = ACCELERATION_RANGE
-        self.follow_road()
         if action[1] == "LANE_LEFT":
             _from, _to, _id = self.target_lane_index
             target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
