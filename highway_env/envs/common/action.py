8d7f39cf1b478aafa666d14a8cac8f3bb67c4777
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-14 11:15:09 +0200

    Update action.py

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -155,7 +155,7 @@ class ContinuousAction_s(ActionType):
                     and self.lateral:
                   actions[1] = 'LANE_RIGHT'
             
-            self.controlled_vehicle.act({"steering": np.clip(ControlledVehicle.steering_control(ControlledVehicle.target_lane_index), 
+            self.controlled_vehicle.act({"steering": np.clip(ControlledVehicle.steering_control(ControlledVehicle.act.target_lane_index), 
                                                              -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE),
                   "acceleration": utils.lmap(actions[0], [-1, 1], self.acceleration_range)})     
         self.last_actions = actions
