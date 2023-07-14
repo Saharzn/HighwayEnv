8b41d05af8efb81a34e78e8b6566c82776d7f7d7
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-14 14:53:59 +0200

    Update action.py

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -174,8 +174,8 @@ class ContinuousAction_s(ActionType):
                 self.target_lane_index = target_lane_index
         elif action[1] == "LANE_RIGHT":
             _from, _to, _id = self.target_lane_index
-            target_lane_index = _from, _to, np.clip(_id + 1, 0, len(self.road.network.graph[_from][_to]) - 1)
-            if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
+            target_lane_index = _from, _to, np.clip(_id + 1, 0, len(network.graph[_from][_to]) - 1)
+            if network.get_lane(target_lane_index).is_reachable_from(self.position):
                 self.target_lane_index = target_lane_index    
         return target_lane_index
 
