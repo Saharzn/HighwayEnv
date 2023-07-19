9dc3205efcba76093c3e530fbbb654c51838ed3c
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-19 14:07:55 +0200

    Update action.py

--- highway_env/envs/common/action.py
+++ highway_env/envs/common/action.py
@@ -241,7 +241,7 @@ class DiscreteMetaAction(ActionType):
         return functools.partial(MDPVehicle, target_speeds=self.target_speeds)
 
     def act(self, action: Union[int, np.ndarray]) -> None:
-        print(self.actions[int(action))
+        print(self.actions[int(action)])
         self.controlled_vehicle.act(self.actions[int(action)])
 
     def get_available_actions(self) -> List[int]:
