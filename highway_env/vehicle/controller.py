4d04068f284b750976cf799c615d710df68521ef
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-19 12:16:18 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -107,7 +107,7 @@ class ControlledVehicle(Vehicle):
             self.target_speed -= self.DELTA_SPEED
             target_lane_index = self.lane_index
         
-        elif action == 4:
+        elif action == 554:
             print("Hi")
             self.target_speed -= self.DELTA_SPEED
             _from, _to, _id = self.target_lane_index
