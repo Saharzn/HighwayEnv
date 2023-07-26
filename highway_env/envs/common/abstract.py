a0643187b1eb62930d4375c6a200e7ded308d1ad
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-26 15:49:28 +0200

    Update abstract.py

--- highway_env/envs/common/abstract.py
+++ highway_env/envs/common/abstract.py
@@ -179,7 +179,7 @@ class AbstractEnv(gym.Env):
             "action": action,
             #"l_index": self.vehicle.lane_index,
             #"target_l_index": self.vehicle.target_lane_index,
-            "fuel": nnv.HighwayEnv.fuel_weight*(self.fuel(action)),
+            "fuel": -2*(self.fuel(action)),
         }
         try:
             info["rewards"] = self._rewards(action)
