e7023cd260c053264f88c7241646ccbf31306ce2
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-07-31 11:08:56 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -123,7 +123,7 @@ class HighwayEnv(AbstractEnv):
             F1 = abs(0.02975+9.162e-06*n+0.004067*T+ 2.752e-08*n**2+6.902e-06*n*T+0.0004899*T**2)
         elif T >= 0:
             F1 = 1.002-0.0004763*n-0.01355*T+7.58e-08*n**2+8.659e-06*n*T+4.649e-05*T**2  
-        if self.vehicle.speed < 27.7:
+        if self.vehicle.speed < = 27.7000:
             F2 = 12.95-1.48*self.vehicle.speed+0.05435*self.vehicle.speed**2-0.000805*self.vehicle.speed**3
         else:
             F2 = 12.95
