a0d6b9b3fd35e8132b439705d4410e055a096f55
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-08-03 21:24:45 +0200

    Update abstract.py

--- highway_env/envs/common/abstract.py
+++ highway_env/envs/common/abstract.py
@@ -174,11 +174,13 @@ class AbstractEnv(gym.Env):
         :return: info dict
         """
         info = {
-            "speed": self.vehicle.speed,
-            "crashed": self.vehicle.crashed,
-            "action": action,
-            "acc": self.ac_sahar(action),
-            "target_speed": self.target_sahar(action),
+            #"speed": self.vehicle.speed,
+            #"crashed": self.vehicle.crashed,
+            #"action": action,
+            #"acc": self.ac_sahar(action),
+            #"target_speed": self.target_sahar(action),
+            "time": self.time, 
+            "fuel": self.fuel(action),
         }
         try:
             info["rewards"] = self._rewards(action)
@@ -186,6 +188,32 @@ class AbstractEnv(gym.Env):
             pass
         return info
 
+
+
+    def fuel(self, action: Action):
+        max_fuel_1 = 20
+        max_fuel_2 = 10
+        max_torque = 230
+        min_torque = -52
+        m = 1400.04
+        ro = 1.206
+        s = 2.414
+        cx = 0.285
+        g = 9.8
+        f = 0.02
+        i = 5.944
+        eta = 0.988
+        r = 0.326
+        n = 30/3.14*i*self.vehicle.speed/r
+        a = self.ac_sahar(action)
+        T_unlimited = m*r/(i*eta)*(a+1/(2*m)*ro*s*cx*self.vehicle.speed**2+g*f)
+        T = np.clip(T_unlimited, min_torque, max_torque)
+        if T < 0:
+            F1 = abs(0.02975+9.162e-06*n+0.004067*T+ 2.752e-08*n**2+6.902e-06*n*T+0.0004899*T**2)
+        elif T >= 0:
+            F1 = 1.002-0.0004763*n-0.01355*T+7.58e-08*n**2+8.659e-06*n*T+4.649e-05*T**2  
+        F2 = -0.0008051*self.vehicle.speed**3+0.05435*self.vehicle.speed**2-1.148*self.vehicle.speed+12.95
+        return F1/max_fuel_1+F2/max_fuel_2
     
     
     
