315f1637725ce80977616984747aa92e6b9d7d18
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-08-08 11:52:27 +0200

    Update abstract.py

--- highway_env/envs/common/abstract.py
+++ highway_env/envs/common/abstract.py
@@ -181,7 +181,7 @@ class AbstractEnv(gym.Env):
             "target_speed": self.target_sahar(action),
             "time": self.time, 
             "fuel": self.fuel(action),
-            "pos": self.vehicle.position,
+            "pos": self.vehicle.position[0],
         }
         try:
             info["rewards"] = self._rewards(action)
