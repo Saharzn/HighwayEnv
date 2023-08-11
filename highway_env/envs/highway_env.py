2a2e21de778156a37d5e7032914c60b7287a7903
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-08-11 09:00:01 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -139,7 +139,7 @@ class HighwayEnv(AbstractEnv):
             "right_lane_reward": lane / max(len(neighbours) - 1, 1),
             "high_speed_reward": np.clip(scaled_speed, 0, 1),
             "on_road_reward": float(self.vehicle.on_road),
-            "fuel_reward": -1.1*self.fuel(action)
+            "fuel_reward": -*self.fuel(action)
         }
      
     def ac_sahar(self, action) -> None:
