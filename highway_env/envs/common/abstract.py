f8f668203ebf9856cb12b908e41337117ae53cf1
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-09-04 11:33:50 +0200

    Update abstract.py

--- highway_env/envs/common/abstract.py
+++ highway_env/envs/common/abstract.py
@@ -192,7 +192,7 @@ class AbstractEnv(gym.Env):
 
 
     def fuel(self, action: Action):
-        max_fuel_1 = 20
+        max_fuel_1 = 15
         max_fuel_2 = 10
         max_torque = 230
         min_torque = -52
