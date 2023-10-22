a8b2d8eb7a608065c6f2f3608f8766ed5d57ee12
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-10-22 17:45:43 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -131,6 +131,7 @@ class HighwayEnv(AbstractEnv):
             F1 = abs(0.7119-0.147*self.vehicle.speed-0.0002227*Force+0.007622*self.vehicle.speed**2+3.697e-5*Force*self.vehicle.speed+1.704e-8*Force**2)
             
         F2 = -0.0008051*self.vehicle.speed**3+0.05435*self.vehicle.speed**2-1.148*self.vehicle.speed+12.95
+        global Z 
         if Z == 0:
             F11 = 10**100
         else:
