6e8adbed778e878c18d312e3d060e93d83e31414
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-10-22 17:25:49 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -133,7 +133,7 @@ class HighwayEnv(AbstractEnv):
         F2 = -0.0008051*self.vehicle.speed**3+0.05435*self.vehicle.speed**2-1.148*self.vehicle.speed+12.95
         if self.time <= 80:
             F11 = 10**100
-        elif self.time > 80:
+        elif self.time+80 >= 80:
             F11 = F1 
         return F11, F2
     
