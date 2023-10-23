9497dae178849d0e4319492ce467b6932a25313d
Saharzn <102530728+Saharzn@users.noreply.github.com>
2023-10-23 09:49:32 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -133,10 +133,12 @@ class HighwayEnv(AbstractEnv):
             
         F2 = -0.0008051*self.vehicle.speed**3+0.05435*self.vehicle.speed**2-1.148*self.vehicle.speed+12.95
         if self.config["Z"] == 0:
-            F11 = 10**100
+            F11 = 0
+            F22 = 0
         else:
-            F11 = F1 
-        return F11, F2
+            F11 = F1
+            F22 = F2
+        return F11, F22
     
     
     
@@ -153,7 +155,8 @@ class HighwayEnv(AbstractEnv):
             "right_lane_reward": lane / max(len(neighbours) - 1, 1),
             "high_speed_reward": np.clip(scaled_speed, 0, 1),
             "on_road_reward": float(self.vehicle.on_road),
-            "fuel_reward": 5/self.fuel(action)[0]
+            "fuel_reward": self.fuel(action)[0]/20+self.fuel(action)[1]/10
+            #"fuel_reward": 5/self.fuel(action)[0]
         }
      
     def ac_sahar(self, action) -> None:
