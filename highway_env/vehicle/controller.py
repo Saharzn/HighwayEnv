9b83eea76924611306336d40c73ff6f61cf1f3b8
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-04-02 12:00:02 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -641,26 +641,13 @@ class ControlledVehicle(Vehicle):
         STEERING_RANGE = (-np.pi / 4, np.pi / 4)
       
         s = 0
-            
-        # elif action[1]>= 0 and action[1]<1 and ControlledVehicle.on_road:
-        #   self.target_lane_index = ("0","1",0)
-        #   s =  np.clip(self.steering_control(self.target_lane_index), -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE)
-            
-        # elif action[1]>=1 and action[1]<2 and ControlledVehicle.on_road:
-        #   self.target_lane_index = ("0","1",1)
-        #   s =  np.clip(self.steering_control(self.target_lane_index), -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE)
-            
-        # elif action[1]>=2 and ControlledVehicle.on_road:
-        #   self.target_lane_index = ("0","1",2)
-        #   s =  np.clip(self.steering_control(self.target_lane_index), -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE)
-      
       
         if action[1]<-0.05:  
           
           #change to left
           _from, _to, _id = self.target_lane_index
           target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
-          if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
+          if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position) and self.vehicle.on_road:
             self.target_lane_index = target_lane_index
             s =  self.steering_control(self.target_lane_index)
 
@@ -669,7 +656,7 @@ class ControlledVehicle(Vehicle):
           # change to right 
           _from, _to, _id = self.target_lane_index
           target_lane_index = _from, _to, np.clip(_id + 1, 0, len(self.road.network.graph[_from][_to]) - 1)
-          if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
+          if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position) and vehicle.on_road:
             self.target_lane_index = target_lane_index
             s = self.steering_control(self.target_lane_index)
             
