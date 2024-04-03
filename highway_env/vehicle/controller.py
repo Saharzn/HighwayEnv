cedb50eb1511c74c6594ffa478156b018525e6ff
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-04-03 11:34:07 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -645,7 +645,6 @@ class ControlledVehicle(Vehicle):
         # middle lane
         if self.lane_index[2] == 1 and action[1]<-0.05:          
           #change to left
-          print("Hi1")
           _from, _to, _id = self.target_lane_index
           target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
           if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
@@ -655,7 +654,6 @@ class ControlledVehicle(Vehicle):
 
         elif self.lane_index[2] == 1 and action[1]>0.05: 
           # change to right 
-          print("Hi2")
           _from, _to, _id = self.target_lane_index
           target_lane_index = _from, _to, np.clip(_id + 1, 0, len(self.road.network.graph[_from][_to]) - 1)
           if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
@@ -664,28 +662,23 @@ class ControlledVehicle(Vehicle):
             
         elif self.lane_index[2] == 1 and action[1] >= -0.05 and action[1]<=0.05:
           s = 0
-          print("Hi3")
           
         # left_lane
         if self.lane_index[2] == 0 and action[1]>0: 
           # change to right 
-          print("Hi4")
           _from, _to, _id = self.target_lane_index
           target_lane_index = _from, _to, np.clip(_id + 1, 0, len(self.road.network.graph[_from][_to]) - 1)
           if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
             self.target_lane_index = target_lane_index
             s = self.steering_control(self.target_lane_index)
         elif self.lane_index[2] == 0  and action[1]<=0:
-            print("Hi5")
             s = 0
             
         # right_lane
         if self.lane_index[2] == 2 and action[1]>=0:
             s = 0
-            print("Hi6")
         elif self.lane_index[2] == 2 and action[1]<0:
             #change to left
-            print("Hi7")
             _from, _to, _id = self.target_lane_index
             target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
             if self.road.network.get_lane(target_lane_index).is_reachable_from(self.position):
