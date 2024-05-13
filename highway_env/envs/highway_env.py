8cf2f9b442811124f7372975c145e8a9c338aff2
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-05-13 16:23:43 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -9,6 +9,7 @@ from highway_env.utils import near_split
 from highway_env.vehicle.controller import ControlledVehicle
 from highway_env.vehicle.kinematics import Vehicle
 from highway_env.vehicle.objects import RoadObject
+from highway_env.road.road import Road, Route, LaneIndex
 
 Observation = np.ndarray
 
