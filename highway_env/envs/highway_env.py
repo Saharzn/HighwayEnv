16dd4aeb43da070ca82bb2bbea03183679c81f1d
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-05-13 17:06:34 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -145,8 +145,8 @@ class HighwayEnv(AbstractEnv):
     
     
     def collision_modified(self,dt): 
-        class_a_instance = RoadObject(self.road, vehicle.position, vehicle.heading, vehicle.speed)
-        class_b_instance = ControlledVehicle(self.controlled_vehicle.road,self.controlled_vehicle.position)
+        class_a_instance = RoadObject(self.road, self.vehicle.position, self.vehicle.heading, self.vehicle.speed)
+        class_b_instance = ControlledVehicle(self.road,self.vehicle.position)
         # Longitudinal: IDM
         front_vehicle, rear_vehicle = self.road.neighbour_vehicles(self, vehicle.lane_index)
         # When changing lane, check both current and target lanes
