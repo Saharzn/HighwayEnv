4a155c23e0a3b3942e97e133262053c1079eeab4
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-05-14 10:27:32 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -147,7 +147,7 @@ class HighwayEnv(AbstractEnv):
     
     def collision_modified(self,dt):
         class_a_instance = ControlledVehicle(self.vehicle.road,self.vehicle.position)
-        return class_b_instance.collision_reward(dt)
+        return class_a_instance.collision_reward(dt)
     
     def _rewards(self, action: Action) -> Dict[Text, float]:
         neighbours = self.road.network.all_side_lanes(self.vehicle.lane_index)
