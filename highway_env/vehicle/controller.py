ca69e82d0954aefee8aba9ba2b70306673e836ae
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-05-29 10:04:33 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -659,10 +659,12 @@ class ControlledVehicle(Vehicle):
           self.target_lane_index = target_lane_index
           s = self.steering_control(self.target_lane_index)
             
-        #elif self.lane_index[2] == 1 and action[1] >= -0.5 and action[1]<=0.5 and Vehicle.on_road:
+        elif self.lane_index[2] == 1 and action[1] >= -0.5 and action[1]<=0.5 and Vehicle.on_road:
           #s = self.steering_control(self.lane_index)
-         # s = 0
-          #print()
+         target_lane = self.road.network.get_lane(self.lane_index)
+         lane_coords = target_lane.local_coordinates(self.position)
+         s = 0
+         print(lane_coords[1])
           
         # left_lane
         if self.lane_index[2] == 0 and action[1]>0.5 and Vehicle.on_road: 
@@ -671,15 +673,19 @@ class ControlledVehicle(Vehicle):
           target_lane_index = _from, _to, np.clip(_id + 1, 0, len(self.road.network.graph[_from][_to]) - 1)
           self.target_lane_index = target_lane_index
           s = self.steering_control(self.target_lane_index)
-        #elif self.lane_index[2] == 0  and action[1]<=0.5 and Vehicle.on_road:
-            #s = self.steering_control(self.lane_index)
-         # s = 0
+        elif self.lane_index[2] == 0  and action[1]<=0.5 and Vehicle.on_road:
+         target_lane = self.road.network.get_lane(self.lane_index)
+         lane_coords = target_lane.local_coordinates(self.position)
+         s = 0
+         print(lane_coords[1])
             
         # right_lane
-        #if self.lane_index[2] == 2 and action[1]>=-0.5 and Vehicle.on_road:
-            #s = self.steering_control(self.lane_index)
-         # s = 0
-        if self.lane_index[2] == 2 and action[1]<-0.5 and Vehicle.on_road:
+        if self.lane_index[2] == 2 and action[1]>=-0.5 and Vehicle.on_road:
+         target_lane = self.road.network.get_lane(self.lane_index)
+         lane_coords = target_lane.local_coordinates(self.position)
+         s = 0
+         print(lane_coords[1])
+        elif self.lane_index[2] == 2 and action[1]<-0.5 and Vehicle.on_road:
             #change to left
             _from, _to, _id = self.target_lane_index
             target_lane_index = _from, _to, np.clip(_id - 1, 0, len(self.road.network.graph[_from][_to]) - 1)
