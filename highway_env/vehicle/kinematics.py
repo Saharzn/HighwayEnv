fc74144e352baf048ce5b754404518b23c8f5f31
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-05-29 15:47:43 +0200

    Update kinematics.py

--- highway_env/vehicle/kinematics.py
+++ highway_env/vehicle/kinematics.py
@@ -129,7 +129,7 @@ class Vehicle(RoadObject):
             self.crashed = True
             self.impact = None
         self.heading += self.speed * np.sin(beta) / (self.LENGTH / 2) * dt
-        if abs(self.heading)<0.005:
+        if abs(self.heading)<0.01:
             self.heading = 0
         self.speed += self.action['acceleration'] * dt
         self.on_state_update()
