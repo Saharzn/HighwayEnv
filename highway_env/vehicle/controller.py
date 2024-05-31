962b501f2ae15472ebd2f0487eb6ba24f09253e5
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-05-31 10:24:35 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -469,9 +469,14 @@ class ControlledVehicle(Vehicle):
 
     """Characteristic time"""
     TAU_ACC = 0.6  # [s]
-    TAU_HEADING = 0.2  # [s]
-    TAU_LATERAL = 0.6  # [s]
+    #TAU_HEADING = 0.2  # [s]
+    #TAU_LATERAL = 0.6  # [s]
 
+
+    TAU_HEADING = 100  # [s]
+    TAU_LATERAL = 150  # [s]
+
+  
     TAU_PURSUIT = 0.5 * TAU_HEADING  # [s]
     KP_A = 1 / TAU_ACC
     KP_HEADING = 1 / TAU_HEADING
