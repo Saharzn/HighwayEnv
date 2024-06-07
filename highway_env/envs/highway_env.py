83f04a7be3c548259e45ba10cf91ca6362766dfb
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-07 15:47:09 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -167,6 +167,7 @@ class HighwayEnv(AbstractEnv):
         # Use forward speed rather than speed, see https://github.com/eleurent/highway-env/issues/268
         forward_speed = self.vehicle.speed * np.cos(self.vehicle.heading)
         scaled_speed = utils.lmap(forward_speed, self.config["reward_speed_range"], [0, 1])
+        print(self.collision_modified(0.1))
         return {
             #"collision_reward": float(self.vehicle.crashed),
             "collision_reward": 1*self.collision_modified(0.1),
@@ -174,8 +175,7 @@ class HighwayEnv(AbstractEnv):
             "high_speed_reward": np.clip(scaled_speed, 0, 1),
             "on_road_reward": float(self.vehicle.on_road),
             "fuel_reward": -self.fuel(action),
-            "centerlane": -4*abs(self.center_lane_reward()),
-            print(self.collision_modified(0.1))
+            "centerlane": -4*abs(self.center_lane_reward())
         }
      
 
