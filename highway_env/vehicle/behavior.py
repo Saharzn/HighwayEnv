85ca4544b0b1911c2e9ac94304912e6bbf37c7bc
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-10 10:53:56 +0200

    Update behavior.py

--- highway_env/vehicle/behavior.py
+++ highway_env/vehicle/behavior.py
@@ -1,11 +1,11 @@
-from typing import Tuple, Union
+from typing import Union
 
 import numpy as np
 
-from highway_env.road.road import Road, Route, LaneIndex
+from highway_env import utils
+from highway_env.road.road import LaneIndex, Road, Route
 from highway_env.utils import Vector
 from highway_env.vehicle.controller import ControlledVehicle
-from highway_env import utils
 from highway_env.vehicle.kinematics import Vehicle
 
 
