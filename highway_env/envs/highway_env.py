310d1871d35c23580c30c55d201eb05151ba4fe8
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-11 09:24:06 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -147,7 +147,7 @@ class HighwayEnv(AbstractEnv):
     
     def collision_modified(self,action:Action):
         class_a_instance = IDMVehicle(self.road,self.vehicle.position)
-        d = class_a_instance.act(action)
+        d = class_a_instance.collision_reward(action)
         print(d)
         if (d<=8):
             return self.config["collision_reward"]*(8-d)/8
