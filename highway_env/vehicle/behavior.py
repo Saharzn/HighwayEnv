64019c469de24fa7bb66d8523f2a5443cb027431
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-11 13:18:18 +0200

    Update behavior.py

--- highway_env/vehicle/behavior.py
+++ highway_env/vehicle/behavior.py
@@ -115,7 +115,7 @@ class IDMVehicle(ControlledVehicle):
             d = self.lane_distance_to(front_vehicle)
         else:
             d = 1000
-        print("hh:", self.position)
+        print("hh:", d)
         Vehicle.act(self, action)  # Skip ControlledVehicle.act(), or the command will be overriden.
         return 5
 
