7f73c12ea3bd54e089fac5f4a7b3bd8606f7e508
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-11 14:35:40 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -172,7 +172,7 @@ def collision_modified(self,dt):
         #"collision_reward": 1*self.collision_modified(),
         return {
             #"collision_reward": float(self.vehicle.crashed),
-            "collision_reward": 1*self.collision_modified(action),
+            "collision_reward": self.collision_modified(0.1),,
             "right_lane_reward": lane / max(len(neighbours) - 1, 1),
             "high_speed_reward": np.clip(scaled_speed, 0, 1),
             "on_road_reward": float(self.vehicle.on_road),
