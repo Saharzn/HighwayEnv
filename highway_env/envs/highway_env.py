13378fd89e3d70afba4b97dea46f490f5dd0624f
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-12 09:21:30 +0200

    Update highway_env.py

--- highway_env/envs/highway_env.py
+++ highway_env/envs/highway_env.py
@@ -149,7 +149,7 @@ class HighwayEnv(AbstractEnv):
     
     def collision_modified(self,dt):
         #class_a_instance = IDMVehicle(self.vehicle.road,self.vehicle.position)
-        class_a_instance = ContinuousAction(self.env)
+        class_a_instance = ContinuousAction(env:AbstractEnv)
         #d = class_a_instance.collision_reward(self.vehicle)
         d = class_a_instance.collision_reward(self.vehicle)
         print(d)
