17a385bba03e2ba62a9b3605a6c209a0d8b0e9d0
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-25 11:56:16 +0200

    Update kinematics.py

--- highway_env/vehicle/kinematics.py
+++ highway_env/vehicle/kinematics.py
@@ -18,7 +18,7 @@ class Vehicle(RoadObject):
     It's state is propagated depending on its steering and acceleration actions.
     """
 
-    LENGTH = 4.0
+    LENGTH = 5.0
     """ Vehicle length [m] """
     WIDTH = 2.0
     """ Vehicle width [m] """
@@ -129,7 +129,8 @@ class Vehicle(RoadObject):
             self.position += self.impact
             self.crashed = True
             self.impact = None
-        self.heading += self.speed * np.sin(beta) / (self.LENGTH / 2) * dt
+        #self.heading += self.speed * np.sin(beta) / (self.LENGTH / 2) * dt
+        self.heading += self.speed * np.sin(beta) / (4 / 2) * dt
         if abs(self.heading)<0.02:
             self.heading = 0
         self.speed += self.action['acceleration'] * dt
