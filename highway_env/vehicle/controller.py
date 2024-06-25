3815ff77aa995f808aa6b96dfbd82f8f44e021c7
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-25 13:29:46 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -454,7 +454,7 @@ from highway_env.road.road import LaneIndex, Road, Route
 from highway_env.utils import Vector
 from highway_env.vehicle.kinematics import Vehicle
 from highway_env.vehicle.objects import RoadObject
-from highway_env.vehicle.controller import MDPVehicle
+from highway_env.vehicle.behavior import IDMVehicle
 
 
 class ControlledVehicle(Vehicle):
