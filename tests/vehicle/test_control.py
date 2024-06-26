40eec62bac4860c597b9d592d0524d503880a7ba
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-26 10:02:54 +0200

    Update test_control.py

--- tests/vehicle/test_control.py
+++ tests/vehicle/test_control.py
@@ -4,7 +4,7 @@ from highway_env.road.lane import StraightLane
 from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.controller import ControlledVehicle
 
-FPS = 15
+FPS = 20
 
 
 def test_step():
