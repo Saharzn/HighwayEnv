8ad8c3c831c66d755466c608830a504c54c5e398
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-26 10:06:14 +0200

    Update test_dynamics.py

--- tests/vehicle/test_dynamics.py
+++ tests/vehicle/test_dynamics.py
@@ -4,7 +4,7 @@ from highway_env.road.road import Road, RoadNetwork
 from highway_env.vehicle.kinematics import Vehicle
 from highway_env.vehicle.objects import Obstacle, Landmark
 
-FPS = 20
+FPS = 15
 
 
 def test_step():
