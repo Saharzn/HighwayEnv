058952ab128f5523aacf2d8e795333403c4df131
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-06-26 11:19:06 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -894,7 +894,7 @@ class MDPVehicle(ControlledVehicle):
         states = []
         v = copy.deepcopy(self)
         t = 0
-        dt = 0.05
+        dt = 0.01
         for action in actions:
             v.act(action)  # High-level decision
             for _ in range(int(action_duration / dt)):
