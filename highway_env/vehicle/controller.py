a6b1a9dd94aec763cb0d2b0659a935c1eb298d42
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-07-05 11:26:33 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -473,8 +473,8 @@ class ControlledVehicle(Vehicle):
     #TAU_HEADING = 0.2  # [s]
     #TAU_LATERAL = 0.6  # [s]
     
-    TAU_HEADING = 2  # [s]
-    TAU_LATERAL = 6  # [s]
+    TAU_HEADING = 20  # [s]
+    TAU_LATERAL = 60  # [s]
   
     TAU_PURSUIT = 0.5 * TAU_HEADING  # [s]
     KP_A = 1 / TAU_ACC
