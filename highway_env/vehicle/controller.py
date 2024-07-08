6721a2c4587ebae1a4035a77b582a401de8ec75f
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-07-08 17:30:58 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -639,7 +639,7 @@ class ControlledVehicle(Vehicle):
 
     def steering_control_EV(self, target_lane_index: LaneIndex) -> float: 
         TAU_HEADING_EV = 0.05  # [s]
-        TAU_LATERAL_EV = 0.05  # [s] 
+        TAU_LATERAL_EV = 0.03  # [s] 
         TAU_PURSUIT_EV = 1 * TAU_HEADING_EV  # [s]
       
         KP_HEADING_EV = 1 / TAU_HEADING_EV
