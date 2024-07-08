0bfecbf2ba4f2c97231fe2d55a419d9fa05e1da7
Saharzn <102530728+Saharzn@users.noreply.github.com>
2024-07-08 23:43:07 +0200

    Update controller.py

--- highway_env/vehicle/controller.py
+++ highway_env/vehicle/controller.py
@@ -671,7 +671,7 @@ class ControlledVehicle(Vehicle):
                 1,
             )
         )
-        steering_angle = np.arctan(2 * np.tan(slip_angle))+0.02*lane_coords[1]
+        steering_angle = np.arctan(2 * np.tan(slip_angle))-0.02*lane_coords[1]
         steering_angle = np.clip(
             steering_angle, -self.MAX_STEERING_ANGLE, self.MAX_STEERING_ANGLE
         )
